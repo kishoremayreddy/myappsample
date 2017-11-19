ff6c95e33efd30ec62b476d0ea7770d278f4814c
kishore <kishoremayreddy@gmail.com>
2017-11-18 21:15:43 -0600

    changes done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 console.log("hello");
-<<<<<<< HEAD
+
 console.log("charan");
 console.log("kishore");
 console.log("fuck");
 console.log("hey");
-=======
+
 console.log("Hello!! This is Charan!!");
